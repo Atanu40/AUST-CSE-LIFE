f05eb5d4ed680f111106afe0010f58a16148d1a6
Atanu <mightyapon98@gmail.com>
2022-02-01 01:59:34 +0600

    PRACTICE 03

---
+++ CSE 3.2/SOFTWARE DEVELOPMENT V/C# PRECTICE CODE/IOOP.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public interface IOOP
+    {
+        void StudentInfo();
+    }
+
+    public class Student
+    {
+        //https://www.w3schools.com/cs/cs_properties.asp
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public string Section { get; set; }
+    }
+}
