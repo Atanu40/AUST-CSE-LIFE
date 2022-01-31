176441f044b401b11e21bef581c3fc624a5da95a
Atanu <mightyapon98@gmail.com>
2022-02-01 01:58:50 +0600

    PRACRICE 01

---
+++ CSE 3.2/SOFTWARE DEVELOPMENT V/C# PRECTICE CODE/Generic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Generic
+{
+    //Generics allow us to write a class or method that can work with any data type.
+    public void ExploreGeneric()
+    {
+        Console.WriteLine(Sum<int>(1, 2));
+        Console.WriteLine(Sum<double>(2.9, 3.9));
+        Console.WriteLine(Sum<string>("10", "20"));
+
+        Console.ReadLine();
+    }
+
+    public static double Sum<T>(T num1, T num2)
+    {
+        double number1 = Convert.ToDouble(num1);
+        double number2 = Convert.ToDouble(num2);
+
+        return number1 + number2;
+    }
+}
