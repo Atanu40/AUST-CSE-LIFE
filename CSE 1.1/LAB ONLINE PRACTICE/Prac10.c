a37b11ae6b600be26bd349a49cbc631ecb35deb4
Atanu <mightyapon98@gmail.com>
2022-04-12 22:30:32 +0600

    Prac 10

---
+++ CSE 1.1/LAB ONLINE PRACTICE/Prac10.c
@@ -0,0 +1,39 @@
+/* Write a program to input the names of ‘n’ guests from user. Then
+find and display the total number of male and female guests from the list as
+well as display the longest (based on character count) name among all. Assume
+that the names will start with Mr. and Mrs. for male and female guests
+respectively. */
+#include <stdio.h>
+#include <string.h>
+int main ()
+{
+ int i , max=0 , n,female=0,male=0 ;
+ printf("How many guests ?\nAns:");
+ scanf("%d",&n);
+ char ch [n][50];
+ getchar();
+ printf("Enter Guests name: \n");
+ for (i=0;i<n;i++){
+ gets(ch[i]);
+ }
+ for (i=0;i<n;i++){
+ if(max<strlen(ch[i])){
+ max=strlen(ch[i]);
+ }
+ if(ch[i][2]=='.'){
+ male++;
+ }
+ if(ch[i][3]=='.'){
+ female++;
+ }
+ }
+ printf("The longest name: \n");
+ for (i=0;i<n;i++){
+ if(max==strlen(ch[i])){
+ puts(ch[i]);
+ }
+ }
+ printf("The female guest: %d\nThe male guest: %d\n",female,male);
+ return 0;
+}
+}
