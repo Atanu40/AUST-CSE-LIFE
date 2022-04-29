7012512a42b9ae56445860b8786eea60fc552fff
Atanu <mightyapon98@gmail.com>
2022-04-29 19:44:11 +0600

    Lab 06 & 07

---
+++ CSE 1.2/JAVA LAB/LAB 06 & 07/Test Overriding/Bank.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testoverriding;
+
+
+public class Bank {
+    
+    int getRateOfInterest() {
+        return 0;
+    }
+}
