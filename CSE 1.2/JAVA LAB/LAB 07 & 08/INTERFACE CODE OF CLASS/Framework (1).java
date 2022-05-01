881d2ab14dddab1210d8c4a6ee7cf3a6e5fd9d06
Atanu <mightyapon98@gmail.com>
2022-05-01 13:26:35 +0600

    LAB 07 & 08

---
+++ CSE 1.2/JAVA LAB/LAB 07 & 08/INTERFACE CODE OF CLASS/Framework (1).java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication3;
+
+/**
+ *
+ * @author student
+ */
+public interface Framework {
+   
+    public void doRouting();
+    public void install();
+}
