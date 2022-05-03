0337933724497566ad7eb834c76154f93b66f1bf
Atanu <mightyapon98@gmail.com>
2022-05-03 11:33:29 +0600

    Lab 07 & 08

---
+++ CSE 1.2/JAVA LAB/LAB 07 & 08/INTERFACE CODE OF CLASS/Language (1).java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication3;
+
+/**
+ *
+ * @author student
+ */
+public interface Language {
+    
+    public void openIDE();
+    public void compile();
+    public void build();
+    public void runCode();
+}
