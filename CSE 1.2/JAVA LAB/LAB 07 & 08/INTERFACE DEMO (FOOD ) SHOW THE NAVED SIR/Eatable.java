d115a4eafbf438ecbc3ebf4f05d5d56e5246de20
Atanu <mightyapon98@gmail.com>
2022-05-04 12:00:36 +0600

    LAB 07 & 08

---
+++ CSE 1.2/JAVA LAB/LAB 07 & 08/INTERFACE DEMO (FOOD ) SHOW THE NAVED SIR/Eatable.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package foodinterface;
+
+/**
+ *
+ * @author student
+ */
+public interface Eatable {
+
+    public void gather();
+    public void cook();
+    public void serve();
+}
