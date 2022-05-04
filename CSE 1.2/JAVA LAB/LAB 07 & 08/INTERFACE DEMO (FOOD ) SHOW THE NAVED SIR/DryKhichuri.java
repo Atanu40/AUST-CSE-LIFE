fc0f9777689e436369c76798ec61e14bd331a6bd
Atanu <mightyapon98@gmail.com>
2022-05-04 12:00:12 +0600

    LAB 07 & 08

---
+++ CSE 1.2/JAVA LAB/LAB 07 & 08/INTERFACE DEMO (FOOD ) SHOW THE NAVED SIR/DryKhichuri.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foodinterface;
+
+/**
+ *
+ * @author Asus
+ */
+public class DryKhichuri extends Khichuri{
+
+    @Override
+    public void cook() {
+        System.out.println("Dry out. Give some peas. Warm it up");
+        System.out.println("Your Dry khichuri is ready");
+        
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
