522b1b0cb92caaa923b6da088f98b5333b63f9a7
Atanu <mightyapon98@gmail.com>
2022-05-06 12:58:08 +0600

    Lab 07 & 08

---
+++ CSE 1.2/JAVA LAB/LAB 07 & 08/INTERFACE DEMO (FOOD ) SHOW THE NAVED SIR/JogaKhichuri.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package foodinterface;
+
+/**
+ *
+ * @author student
+ */
+public class JogaKhichuri extends Khichuri{
+
+    @Override
+    public void cook() {
+        System.out.println("Mix more things like vegetables and piece of meats, keep a bit of water");
+        System.out.println("Your Jogakhichuri is ready");
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
