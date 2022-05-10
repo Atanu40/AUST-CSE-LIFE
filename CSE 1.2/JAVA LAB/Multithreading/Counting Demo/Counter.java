02a77dec642d21f1b2ec903579a9da761642714a
Atanu <mightyapon98@gmail.com>
2022-05-10 20:02:47 +0600

    Multithreading

---
+++ CSE 1.2/JAVA LAB/Multithreading/Counting Demo/Counter.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package countingdemo;
+
+/**
+ *
+ * @author student
+ */
+public class Counter {
+    
+    static int item=0;
+    
+    int incItem(){
+        
+        item++;
+        return item;
+    }
+    
+}
