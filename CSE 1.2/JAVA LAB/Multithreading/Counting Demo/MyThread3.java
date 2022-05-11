da278cfccd79b192afe71570297e8b92d74c1f58
Atanu <mightyapon98@gmail.com>
2022-05-11 20:29:48 +0600

    MultiThread

---
+++ CSE 1.2/JAVA LAB/Multithreading/Counting Demo/MyThread3.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package countingdemo;
+
+/**
+ *
+ * @author student
+ */
+public class MyThread3 implements Runnable{
+    
+    Counter c;
+    
+    MyThread3(Counter c)
+    {
+        this.c=c;
+    }
+   
+    public void run() {
+        
+        for(int i=1;i<=5;i++){
+              System.out.println(c.incItem());
+        }
+       
+    }
+}
