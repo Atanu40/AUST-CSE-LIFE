18b2f506ecac16c2e6bcdc16edaa466450ae7690
Atanu <mightyapon98@gmail.com>
2022-05-19 11:33:10 +0600

    Multithread

---
+++ CSE 1.2/JAVA LAB/Multithreading/ThreadAnother/OddPrinter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package threadanother;
+
+/**
+ *
+ * @author student
+ */
+public class OddPrinter {
+    
+    public static void print()
+    {
+    for(int i=1;i<10000;i+=2)
+        System.out.println("Printing odd"+i);
+    
+    
+    }
+}
