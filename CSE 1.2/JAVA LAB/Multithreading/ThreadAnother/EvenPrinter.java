aa370cb209997bfb87e54a7e23d63d1191cad488
Atanu <mightyapon98@gmail.com>
2022-05-19 11:32:04 +0600

    MultiThread

---
+++ CSE 1.2/JAVA LAB/Multithreading/ThreadAnother/EvenPrinter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package threadanother;
+
+/**
+ *
+ * @author student
+ */
+public class EvenPrinter {
+    
+    public static void print()
+    {
+    for(int i=2;i<10000;i+=2)
+        System.out.println("Printing even"+i);
+    
+    
+    }
+}
