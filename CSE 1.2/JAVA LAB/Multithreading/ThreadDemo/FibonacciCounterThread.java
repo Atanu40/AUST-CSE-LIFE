bac07f7bef8df5512f0de4d0be6e14baaae83156
Atanu <mightyapon98@gmail.com>
2022-05-21 20:05:31 +0600

    Multithread

---
+++ CSE 1.2/JAVA LAB/Multithreading/ThreadDemo/FibonacciCounterThread.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threaddemo;
+
+/**
+ *
+ * @author Asus
+ */
+public class FibonacciCounterThread implements Runnable{
+
+    private int n;
+   
+    public FibonacciCounterThread(int n)
+    {
+    this.n = n;
+    }
+    @Override
+    public void run() {
+        FibonacciCounter.countFibonacci(this.n);
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
