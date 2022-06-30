94c4ad82bfc046dc2cdef22c36c76e46084e59a1
Atanu <mightyapon98@gmail.com>
2022-06-30 13:43:37 +0600

    Offline

---
+++ CSE 2.2/Assembly Lab/Offline 08/Assignment_8_task_1.asm
@@ -0,0 +1,41 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+.CODE
+MAIN PROC
+    MOV AH,2
+    MOV DL,'-'
+    INT 21H
+    MOV AH,2
+    MOV DL,'>'
+    INT 21H
+
+    XOR CX,CX
+    
+    MOV AH,1
+    INT 21H
+    
+    WHILE_:
+    CMP AL,0DH
+    JE END_WHILE
+    PUSH AX
+    INC CX
+    INT 21H
+    JMP WHILE_
+
+    END_WHILE:
+    MOV AH,2
+    MOV DL,0DH
+    MOV DL,0AH
+    INT 21H
+
+    TOP:
+    POP DX
+    INT 21H
+    LOOP TOP
+
+    EXIT:
+    MOV AH,4CH
+    INT 21H
+  MAIN ENDP
+END MAIN
