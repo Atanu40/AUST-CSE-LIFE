4ee8878fac6a042281695d1fa4ab4d21de095d89
Atanu <mightyapon98@gmail.com>
2022-07-21 01:18:52 +0600

    code

---
+++ CSE 1.2/JAVA LAB/Practice problem/Bus.java
@@ -0,0 +1,18 @@
+
+package assinment1;
+
+
+public abstract class Bus {
+    //create an instance variable\\
+    String bus_name;
+    String ac_facility;
+    String wifi_facility;
+    String destianation;
+    int total_seat;
+    int ticket_prize;
+    
+    abstract void businformation();
+      
+    
+    
+}
