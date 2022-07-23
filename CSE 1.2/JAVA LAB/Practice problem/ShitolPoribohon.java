17f9a9ae2144f5bfb4831a7eb511eb61b57417f6
Atanu <mightyapon98@gmail.com>
2022-07-24 01:13:26 +0600

    Practice

---
+++ CSE 1.2/JAVA LAB/Practice problem/ShitolPoribohon.java
@@ -0,0 +1,18 @@
+
+package assinment1;
+
+
+public class ShitolPoribohon  extends Bus{
+    
+     @Override
+    void businformation(){
+        System.out.println("OUR BUS NAME IS : SHITOL PORIBOHON");
+        System.out.println("OUR BUS'S AC FACILITY HAVE : YES");
+        System.out.println("OUR BUS'S WIFI FACILITY HAVE :YES");
+        System.out.println("OUR BUS'S DESTINATION IS : DHAKA TO NARAYANGANJ");
+        System.out.println("THIS COMPANY BUS'S TOTAL SEAT ARE : 100");
+        System.out.println("SIR/MADAM YOUR PER TICKET PRIZE IS : ONLY 100 TK");
+         System.out.println("");
+    }
+    
+}
